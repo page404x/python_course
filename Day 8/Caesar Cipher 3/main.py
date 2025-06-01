9cb8a3e24e2354ca33fbe71315d8ccd1d8c31cf2
Reko Kurniawan <page404@outlook.com>
2025-06-01 17:59:19 +0700

    Day 8 Task 5 Done

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,32 +1,43 @@
 # TODO-1: Import and print the logo from art.py when the program starts.
+from operator import truediv
 
+from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(logo)
 # TODO-2: What happens if the user enters a number/symbol/space?
 
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
-
+    if encode_or_decode == "decode":
+        shift_amount *= -1
     for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
-
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text +=letter
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 
 # TODO-3: Can you figure out a way to restart the cipher program?
+again = True
 
+while again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    choice = input("Do you want to run again (Y/N)").lower()
+    if choice == "y":
+        again = True
+    else:
+        again = False
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
 
 
