03fb17a44ee8b7fafead35bf0763396c96016c0b
Reko Kurniawan <page404@outlook.com>
2025-06-01 13:21:37 +0700

    Day 8 Task 3 finished

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,9 @@
 # Functions with input
 
-def greet_with_name(name):
+def greet_with_name(name, location):
     print(f"Hello {name}")
     print(f"How do you do {name}?")
+    print(f"I'm from {location}")
 
 
-greet_with_name("Jack Bauer")
+greet_with_name("Jack Bauer", "bandung")
