115ba31a92c48abb88250438247eb5cb08615f6a
Reko Kurniawan <page404@outlook.com>
2025-06-01 13:04:38 +0700

    Day 8 - Function with Input

--- Day 8/Functions with Inputs/task.py
+++ Day 8/Functions with Inputs/task.py
@@ -1,1 +1,17 @@
 #Function with inputs
+
+def greet(name):
+    print(f"Hello {name}")
+    print("How do you do?")
+    print("Isn't the weather nice?")
+
+greet("reko")
+
+
+def life_in_weeks(age):
+    years_remaining = 90 - age
+    weeks_remaining = years_remaining * 12 * 4
+    print(weeks_remaining)
+
+age = int(input("Please tell me your age now ? "))
+life_in_weeks(age)
