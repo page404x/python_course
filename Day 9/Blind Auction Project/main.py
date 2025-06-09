76d2ef0ae9473643356302b29a7fa4e9c3f40b6c
Reko Kurniawan <page404@outlook.com>
2025-06-09 13:02:02 +0700

    Finish Day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,33 @@
+from art import logo
+
+
 # TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
+# Input Data
+
+bid_dict = {}
+
+repeat = "y"
+while repeat == "y" :
+    print(logo)
+    name = input("What is your name? ")
+    bid = input("How much is your bid? ")
+
+    # TODO-3: Whether if new bids need to be added
+    repeat = input("Do you want to input your bid again [y/n] ?").lower()
+
+    # TODO-2: Save data into dictionary {name: price}
+    bid_dict[name]= bid
+    print("\n" * 10)
+
 # TODO-4: Compare bids in dictionary
+max_bid = max(bid_dict.values())
+max_name = max(bid_dict, key=bid_dict.get)
+print(f"Winner goes to {max_name} with the highest bid {max_bid}")
+
+
+
+
+
+
 
 
