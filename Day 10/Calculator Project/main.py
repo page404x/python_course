c29891e8c9e7175490b58f4994032c7260297619
Reko Kurniawan <page404@outlook.com>
2025-06-15 16:12:20 +0700

    Finish Day 10 Course

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,30 @@
 def add(n1, n2):
+    """Simple mathematical function to add 2 given numbers"""
     return n1 + n2
 
+def subtract(n1, n2):
+    """A simple mathematical function to subtract 2 given numbers"""
+    return n1 - n2
+
+def multiply(n1, n2):
+    """a simple mathematical function to multiply 2 given numbers"""
+    return n1 * n2
+
+def divide(n1, n2):
+    """a simple matematical function to dived 2 given numbers"""
+    return n1 / n2
+
+math_func = {"+":add, "-" :subtract, "*" : multiply, "/": divide}
+
+repeat = True
+while repeat:
+    x = int(input("What is your first number: "))
+    y = int(input("What is your second number: "))
+    operator_symbol = input("Type a mathematical operator : ")
+    print(f"Mathematical result is : {math_func[operator_symbol](x,y)}")
+    ask = input("Do you want to calculate more? ").lower()
+    if ask == 'y':
+        repeat = True
+    else:
+        repeat = False
 
