b7b1b86946e65aa6b7538c09be042895b39796d3
page404x <page404@outlook.com>
2025-06-21 17:42:20 +0700

    Finishing code day 11

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -9,15 +9,15 @@ def deal_card(limit):
     card = {}
     while counter < limit:
         index = random.randrange(len(cards))
-        card[index] = cards[index]
-        counter +=1
+        if index  in card.keys():
+            counter = counter
+        else:
+            card[index] = cards[index]
+            counter +=1
     return card
 
 def sum_card_value(card_value):
-    total = sum(card_value.values())
-    if total > 21:
-        total %= 21
-    return total
+    return sum(card_value.values())
 
 def showing_card(card):
     show_card =[]
@@ -37,7 +37,13 @@ def showing_card(card):
 
 
 def result(t_c_card, t_p_card):
-    if t_c_card > t_p_card:
+    if t_c_card > 21 and t_p_card > 21:
+        r = "Draw"
+    elif t_c_card > 21:
+        r = "You Win"
+    elif t_p_card > 21:
+        r = "You Lose"
+    elif t_c_card > t_p_card:
         r = "You Lose"
     elif t_c_card < t_p_card:
         r = "You Win"
@@ -45,32 +51,36 @@ def result(t_c_card, t_p_card):
         r = "Draw"
     return r
 
-
-
-cpu_card = deal_card(2)
-total_cpu_card = sum_card_value(cpu_card)
-c_card = showing_card(cpu_card)
-c_card[1] = "***"
-print(f"CPU Card : {c_card}")
-
-
-player_card = deal_card(2)
-total_player_card = sum_card_value(player_card)
-p_card = showing_card(player_card)
-print(f"Player Card : {p_card}")
-
-deal_more = input("Do you want to deal more card? ")
-if deal_more == "y":
-    player_card.update(deal_card(1))
-    total_player_card = sum_card_value(player_card)
-
-if total_cpu_card < 17:
-    print("CPU deal more card.... ")
-    cpu_card.update(deal_card(1))
+play = input("Do you want to play black jack (Y/N)? ").lower()
+while play=="y":
+    cpu_card = deal_card(2)
     total_cpu_card = sum_card_value(cpu_card)
+    c_card = showing_card(cpu_card)
+#    c_card[1] = "***"
+    print(f"CPU Card : {c_card[0]}")
 
-print(f"CPU Card are : {showing_card(cpu_card)}")
-print(f"Player Card are: {showing_card(player_card)}")
-print(f"And The Result is : {result(total_cpu_card, total_player_card)}")
+    player_card = deal_card(2)
+    total_player_card = sum_card_value(player_card)
+    p_card = showing_card(player_card)
+    print(f"Player Card : {p_card}")
 
+    deal_more = input("Do you want to deal more card (Y/N)? ").lower()
+    while deal_more == "y":
+        player_card.update(deal_card(1))
+        total_player_card = sum_card_value(player_card)
+        print(f"Player Card are: {showing_card(player_card)}")
+        if total_player_card <= 21:
+            deal_more = input("Do you want to deal more card (Y/N)? ").lower()
+        else:
+            deal_more = "n"
+
+    while total_cpu_card < 17:
+        print("CPU deal more card.... ")
+        cpu_card.update(deal_card(1))
+        total_cpu_card = sum_card_value(cpu_card)
+
+    print(f"CPU Card are : {showing_card(cpu_card)}")
+    print(f"Player Card are: {showing_card(player_card)}")
+    print(f"And The Result is : {result(total_cpu_card, total_player_card)}")
+    play = input("Do you want to play black jack (Y/N)? ").lower()
 
