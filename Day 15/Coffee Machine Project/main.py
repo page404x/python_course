d625b96169bb63242d78e941ed4fc7928387222d
page404x <page404@outlook.com>
2025-07-19 19:53:19 +0700

    Day 15th

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -30,3 +30,30 @@ resources = {
     "coffee": 100,
 }
 
+#TODO 1 - User Input (Espresso/Latte/Cappucino)
+#user_input = input("What Would You Like? (Espresso/Latte/Cappucino? ").lower()
+
+def coffee_ingredient(choice):
+    coffee = MENU[choice]["ingredients"]["coffee"]
+    return coffee
+
+def water_ingredient(choice):
+    water = MENU[choice]["ingredients"]["water"]
+    return water
+
+def milk_ingredient(choice):
+    milk = MENU[choice]["ingredients"]["milk"]
+    return milk
+
+def coffee_cost(coffee):
+    cost = MENU[coffee]["cost"]
+    return cost
+
+def resource_remaining(coffee, water, milk):
+    remaining_water = resources["water"]-
+
+
+#TODO 2 - Turn off Machine (OFF)
+#TODO 3 - Print Report
+
+#
