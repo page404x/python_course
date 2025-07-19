ec51a808d4f590dd6eb56c28822074d1688cc776
page404x <page404@outlook.com>
2025-07-19 17:09:37 +0700

    Touching up day 14

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -11,16 +11,18 @@ def compare_idx():
 
 score = 0
 lose = False
+print(art.logo)
 while not lose:
-    print(art.logo)
     list_a = compare_idx()
-    print(f"Compare A : {list_a[0]}, {list_a[2]}, from {list_a[3]} {list_a[1]}.")
+    print(f"Compare A : {list_a[0]}, {list_a[2]}, from {list_a[3]}.")
 
     print(art.vs)
     list_b = compare_idx()
-    print(f"Againts B : {list_b[0]}, {list_b[2]}, from {list_b[3]} {list_b[1]}.")
+    print(f"Againts B : {list_b[0]}, {list_b[2]}, from {list_b[3]}.")
 
     choice = input("who has more followers? Type 'A' or 'B':").lower()
+    print("\n" * 20)
+    print(art.logo)
 
     if choice == "a" and (list_a[1] > list_b[1]):
         score += 1
