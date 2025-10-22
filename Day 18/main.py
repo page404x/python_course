4e2a4a7f6f54d7d77d6e1f91d87ff856bdc76a01
page404x <page404@outlook.com>
2025-10-22 19:01:23 +0700

    day 18th Turtle Graphic

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,9 +1,14 @@
 from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("green")
+timmy_the_turtle.color("black")
 canvas = Screen()
+
+
+for i in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
 canvas.exitonclick()
 
 
